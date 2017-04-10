f4ed8e3349551b058bc1d5e48f0f3906960c68b2
IQubic <avi.caspe@gmail.com>
2017-04-09 23:22:19 -0700

    Updated morse.cabal again

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,23 @@
-module Main where
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [ #title := "Hi there" ]
+
+  on win #destroy Gtk.mainQuit
+
+  button <- new Gtk.Button [ #label := "Click me" ]
+
+  on button #clicked (set button [ #sensitive := False,
+                                   #label := "Thanks for clicking me" ])
+
+  #add win button
+
+  #showAll win
+
+  Gtk.main
